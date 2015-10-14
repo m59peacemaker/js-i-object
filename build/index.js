6660632159a05e71d90e9e0525eeb8354a6081f3
Johnny Hauser <johnnyhauser@gmail.com>
2015-10-14 17:11:51 -0500

    used es6 like a smart person

--- build/index.js
+++ build/index.js
@@ -2,39 +2,42 @@
 
 var merge = require('lodash/object/merge');
 
-module.exports = thing;
+module.exports = getObject;
 
-function thing(input) {
-  var _this = this,
-      _arguments = arguments;
+function getObject(input) {
+  for (var _len = arguments.length, params = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+    params[_key - 1] = arguments[_key];
+  }
 
   if (!Array.isArray(input)) {
-    return handleItem.apply(this, arguments);
+    return handleItem.apply(undefined, [input].concat(params));
   }
   var objects = input.map(function (item) {
-    return handleItem.apply(_this, [item].concat([].slice.call(_arguments, 1)));
+    return handleItem.apply(undefined, [item].concat(params));
   });
   objects.unshift({});
   return merge.apply(null, objects);
 }
 
 function handleItem(_x) {
-  var _this2 = this,
-      _arguments2 = arguments;
-
+  var _arguments = arguments;
   var _again = true;
 
   _function: while (_again) {
     var item = _x;
+    _len2 = params = _key2 = undefined;
     _again = false;
 
+    for (var _len2 = _arguments.length, params = Array(_len2 > 1 ? _len2 - 1 : 0), _key2 = 1; _key2 < _len2; _key2++) {
+      params[_key2 - 1] = _arguments[_key2];
+    }
+
     if (Array.isArray(item)) {
-      return thing.apply(_this2, _arguments2);
+      return getObject.apply(undefined, [item].concat(params));
     } else if (typeof item === 'object') {
       return item;
     } else if (typeof item === 'function') {
-      _this2 = undefined;
-      _arguments2 = [_x = item.apply(_this2, [].slice.call(_arguments2, 1)) || {}];
+      _arguments = [_x = item.apply(undefined, params) || {}];
       _again = true;
       continue _function;
     } else {
--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 var merge = require('lodash/object/merge');
 
-module.exports = thing;
+module.exports = getObject;
 
-function thing(input) {
+function getObject(input, ...params) {
   if (!Array.isArray(input)) {
-    return handleItem.apply(this, arguments);
+    return handleItem(input, ...params);
   }
-  var objects = input.map((item) => handleItem.apply(this, [item].concat([].slice.call(arguments, 1))));
+  var objects = input.map((item) => handleItem(item, ...params));
   objects.unshift({});
   return merge.apply(null, objects);
 }
 
-function handleItem(item) {
+function handleItem(item, ...params) {
   if (Array.isArray(item)) {
-    return thing.apply(this, arguments);
+    return getObject(item, ...params);
   } else if (typeof item === 'object') {
     return item;
   } else if (typeof item === 'function') {
-    return handleItem(item.apply(this, [].slice.call(arguments, 1)) || {});
+    return handleItem(item(...params) || {});
   } else {
     throw new Error(typeof  item+' is not supported. Input should be an array, object or function.');
   }
